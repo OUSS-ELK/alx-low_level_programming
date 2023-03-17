e682114d0ca865ca1bdad320337147a814af3a82
oussama elkharraf <elkharrafoussama@gmail.com>
2023-03-17 01:46:22 +0100

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * * main- A program that print with puts function
  * * Return: value 0 (Success)
- * */
+ **/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
