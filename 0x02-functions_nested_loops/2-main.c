2b9c80265b30280d09ba2de4b1049278aebafff3
oussama elkharraf <elkharrafoussama@gmail.com>
2023-03-21 18:13:20 +0000

    update task

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,10 +1,11 @@
+#include "main.h"
 /**
- ** main - check the code.
- ** Return: Always 0.
+ * main - check the code.
+ * Return: Always 0.
  **/
 
 int main(void)
 {
-	iprint_alphabet_x10();
+	print_alphabet_x10();
 	return (0);
 }
